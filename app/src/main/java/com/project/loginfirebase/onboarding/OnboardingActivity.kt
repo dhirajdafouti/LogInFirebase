f17005b27f5ddcdf27543f3ead061f414bf306b8
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-17 22:07:11 +0530

    The firebase onboarding

---
+++ app/src/main/java/com/project/loginfirebase/onboarding/OnboardingActivity.kt
@@ -0,0 +1,11 @@
+package com.project.loginfirebase.onboarding
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OnboardingActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
