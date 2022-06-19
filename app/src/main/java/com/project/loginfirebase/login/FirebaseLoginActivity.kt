3ea1f9da67664f34ab8c2e17a4b145fc84bd6f21
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-19 20:16:43 +0530

    The firebase onboarding screen

---
+++ app/src/main/java/com/project/loginfirebase/login/FirebaseLoginActivity.kt
@@ -0,0 +1,15 @@
+package com.project.loginfirebase.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.loginfirebase.R
+
+class FirebaseLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_login)
+
+
+
+    }
+}
